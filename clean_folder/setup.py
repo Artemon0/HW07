9f96d0f89e2928b6cebe7d667f1a239ce41f98e4
Artemon <artemon888.com@gmail.com>
2023-03-11 14:44:03 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,5 +4,7 @@ setup(
     name='clean_foder',
     version='0.0.1',
     author='Onyshchenko Artem',
-    url='https://github.com/Artemon0/HW07'
+    author_email='artemon888.com@gmail.com',
+    url='https://github.com/Artemon0/HW07',
+    command_packages=['clean_folder = D:\\Python_projects\\HW\\HW6']
 )
