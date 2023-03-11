df23e9d750eef0025c9d50176a7fdea09d09c59c
Artemon <artemon888.com@gmail.com>
2023-03-11 14:51:20 +0200

    1

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     author='Onyshchenko Artem',
     author_email='artemon888.com@gmail.com',
-    url='https://github.com/Artemon0/HW07',
-    command_packages=['clean_folder = D:\\Python_projects\\HW\\HW6\\sort.py']
+    url='https://github.com/Artemon0/HW07'
+    # command_packages=['clean_folder = D:\\Python_projects\\HW\\HW6\\sort.py']
+    command_packages=
 )
